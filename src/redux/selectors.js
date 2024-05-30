cc8475da426053171ebac704386749400bf80c36
czke33 <czke33@gmail.com>
2024-05-30 18:23:55 +0300

    add selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,3 +2,5 @@ export const getContacts = state => state.contacts.items;
 export const getFilter = state => state.filter.value;
 export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
+export const getIsLogged = state => state.auth.isLoggedIn;
+export const getEmail = state => state.auth.email;
