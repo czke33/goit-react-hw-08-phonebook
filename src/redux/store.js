8049aa781d1be8f9b92c248fcd5c3b8d1ff1e47b
czke33 <czke33@gmail.com>
2024-05-30 18:25:55 +0300

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { contactsReducer } from './constactsSlice';
 import { filterReducer } from './filterSlice';
+import { authReducer } from './userSlice';
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
+    auth:authReducer,
   },
 });
